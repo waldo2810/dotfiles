aa8f90c5607e35366a805cce59c522e2aa801bd5
Waldo Sobrino <waldo2810@gmail.com>
2024-06-14 11:54:35 -0500

    kanagawa

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -23,34 +23,50 @@ vim.api.nvim_set_hl(0, 'NvimTreeEndOfBuffer', { bg = 'none' })
 
 return {
     {
-        "ellisonleao/gruvbox.nvim",
+        "rebelot/kanagawa.nvim",
         priority = 1000,
         config = function ()
-            require("gruvbox").setup({
-                terminal_colors = true, -- add neovim terminal colors
-                undercurl = true,
-                underline = true,
-                bold = true,
-                italic = {
-                    strings = false,
-                    emphasis = true,
-                    comments = true,
-                    operators = false,
-                    folds = true,
+            require("kanagawa").setup({
+                commentStyle = { italic = true },
+                keywordStyle = { italic = false},
+                statementStyle = { bold = true },
+                transparent = true,         -- do not set background color
+                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+                theme = "wave",              -- Load "wave" theme when 'background' option is not set
+                background = {               -- map the value of 'background' option to a theme
+                    dark = "wave",           -- try "dragon" !
+                    light = "lotus"
                 },
-                strikethrough = true,
-                invert_selection = false,
-                invert_signs = false,
-                invert_tabline = false,
-                invert_intend_guides = false,
-                inverse = true, -- invert background for search, diffs, statuslines and errors
-                contrast = "", -- can be "hard", "soft" or empty string
-                palette_overrides = {},
-                overrides = {},
-                dim_inactive = false,
-                transparent_mode = true,
+                colors = {
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = "none"
+                            }
+                        }
+                    }
+                }
+
             })
-            vim.cmd("colorscheme gruvbox")
-        end,
-    }
+            vim.cmd("colorscheme kanagawa")
+        end
+    },
+    -- {
+    --     "ellisonleao/gruvbox.nvim",
+    --     priority = 1000,
+    --     config = function ()
+    --         require("gruvbox").setup({
+    --             italic = {
+    --                 strings = false,
+    --                 emphasis = true,
+    --                 comments = true,
+    --                 operators = false,
+    --                 folds = true,
+    --             },
+    --             contrast = "", -- can be "hard", "soft" or empty string
+    --             transparent_mode = true,
+    --         })
+    --         vim.cmd("colorscheme gruvbox")
+    --     end,
+    -- }
 }
