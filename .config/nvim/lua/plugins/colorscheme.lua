37c1cad1b0d5f74e438ef3e7773ddd6631dad10b
Waldo Sobrino <waldo2810@gmail.com>
2024-06-18 18:29:28 -0500

    add rosepine

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,56 +1,79 @@
 -- transparent background
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'Terminal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'FoldColumn', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'Folded', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
-
--- transparent background for neotree
-vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NeoTreeVertSplit', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NeoTreeWinSeparator', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NeoTreeEndOfBuffer', { bg = 'none' })
-
--- transparent background for nvim-tree
-vim.api.nvim_set_hl(0, 'NvimTreeNormal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NvimTreeVertSplit', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NvimTreeEndOfBuffer', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'Terminal', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'FoldColumn', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'Folded', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none' })
+--
+-- -- transparent background for neotree
+-- vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'NeoTreeVertSplit', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'NeoTreeWinSeparator', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'NeoTreeEndOfBuffer', { bg = 'none' })
+--
+-- -- transparent background for nvim-tree
+-- vim.api.nvim_set_hl(0, 'NvimTreeNormal', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'NvimTreeVertSplit', { bg = 'none' })
+-- vim.api.nvim_set_hl(0, 'NvimTreeEndOfBuffer', { bg = 'none' })
 
 return {
     {
-        "rebelot/kanagawa.nvim",
-        priority = 1000,
+        "rose-pine/neovim",
+        name = "rose-pine",
         config = function ()
-            require("kanagawa").setup({
-                commentStyle = { italic = true },
-                keywordStyle = { italic = false},
-                statementStyle = { bold = true },
-                transparent = true,         -- do not set background color
-                terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-                theme = "wave",              -- Load "wave" theme when 'background' option is not set
-                background = {               -- map the value of 'background' option to a theme
-                    dark = "wave",           -- try "dragon" !
-                    light = "lotus"
+            require("rose-pine").setup({
+                dark_variant = "main", -- main, moon, or dawn
+                styles = {
+                    bold = true,
+                    italic = false,
+                    transparency = true,
                 },
-                colors = {
-                    theme = {
-                        all = {
-                            ui = {
-                                bg_gutter = "none"
-                            }
-                        }
-                    }
-                }
-
+                highlight_groups = {
+                    -- Comment = { fg = "foam" },
+                    -- VertSplit = { fg = "muted", bg = "muted" },
+                },
             })
-            vim.cmd("colorscheme kanagawa")
+
+            vim.cmd("colorscheme rose-pine")
+            -- vim.cmd("colorscheme rose-pine-main")
+            -- vim.cmd("colorscheme rose-pine-moon")
+            -- vim.cmd("colorscheme rose-pine-dawn")
         end
     },
+    -- {
+    --     "rebelot/kanagawa.nvim",
+    --     priority = 1000,
+    --     config = function ()
+    --         require("kanagawa").setup({
+    --             commentStyle = { italic = true },
+    --             keywordStyle = { italic = false},
+    --             statementStyle = { bold = true },
+    --             transparent = true,         -- do not set background color
+    --             terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    --             theme = "wave",              -- Load "wave" theme when 'background' option is not set
+    --             background = {               -- map the value of 'background' option to a theme
+    --                 dark = "wave",           -- try "dragon" !
+    --                 light = "lotus"
+    --             },
+    --             colors = {
+    --                 theme = {
+    --                     all = {
+    --                         ui = {
+    --                             bg_gutter = "none"
+    --                         }
+    --                     }
+    --                 }
+    --             }
+    --
+    --         })
+    --         vim.cmd("colorscheme kanagawa")
+    --     end
+    -- },
     -- {
     --     "ellisonleao/gruvbox.nvim",
     --     priority = 1000,
