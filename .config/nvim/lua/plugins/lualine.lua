6ed8b37f200c5d71eb99058c609e8387af6b6081
Waldo Sobrino <waldo2810@gmail.com>
2024-06-28 10:42:49 -0500

    nvim: lualine full path

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -12,7 +12,9 @@ return {
             sections = {
                 lualine_a = {'mode'},
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
+                lualine_c = {
+                    {'filename', path = 1}
+                },
                 lualine_x = {'encoding', 'fileformat', 'filetype'},
                 lualine_z = {'location'}
             },
