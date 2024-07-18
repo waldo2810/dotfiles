4e084db5382aa0b474fedc637344532e7b18bee8
Waldo Sobrino <waldo2810@gmail.com>
2024-07-18 11:53:03 -0500

    nvim: lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,28 +1,29 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function ()
+    config = function()
         require('lualine').setup {
             options = {
                 icons_enabled = true,
                 theme = 'auto',
-                component_separators = { left = '', right = ''},
-                section_separators = { left = '', right = ''},
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
             },
             sections = {
-                lualine_a = {'mode'},
-                lualine_b = {'branch', 'diff', 'diagnostics'},
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
                 lualine_c = {
-                    {'filename', path = 1}
+                    'filename'
+                    -- {'filename', path = 1}
                 },
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_z = {'location'}
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_z = { 'location' }
             },
             inactive_sections = {
                 lualine_a = {},
                 lualine_b = {},
-                lualine_c = {'filename'},
-                lualine_x = {'location'},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
                 lualine_y = {},
                 lualine_z = {}
             },
@@ -31,6 +32,5 @@ return {
             inactive_winbar = {},
             extensions = {}
         }
-
     end
 }
