d487ab63816161bd33551e54f828aac2c661cd45
Waldo Sobrino <waldo2810@gmail.com>
2024-07-23 18:18:34 -0500

    nvim: conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -12,26 +12,31 @@ return { -- Autoformat
             desc = '[F]ormat buffer',
         },
     },
-    opts = {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-            -- Disable "format_on_save lsp_fallback" for languages that don't
-            -- have a well standardized coding style. You can add additional
-            -- languages here or re-enable it for the disabled ones.
-            local disable_filetypes = { c = true, cpp = true }
-            return {
-                timeout_ms = 500,
-                lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-            }
-        end,
-        formatters_by_ft = {
-            lua = { 'stylua' },
-            -- Conform can also run multiple formatters sequentially
-            -- python = { "isort", "black" },
-            --
-            -- You can use a sub-list to tell conform to run *until* a formatter
-            -- is found.
-            -- javascript = { { "prettierd", "prettier" } },
-        },
-    }
+    config = function()
+        require('conform').setup({
+            notify_on_error = true,
+            format_on_save = function(bufnr)
+                -- Disable "format_on_save lsp_fallback" for languages that don't
+                -- have a well standardized coding style. You can add additional
+                -- languages here or re-enable it for the disabled ones.
+                local disable_filetypes = { c = true, cpp = true }
+                return {
+                    timeout_ms = 500,
+                    lsp_format = "fallback",
+                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+                }
+            end,
+            formatters_by_ft = {
+                lua = { 'stylua' },
+                javascript = { { "biome", "prettierd", "prettier" } },
+                typescript = { { "biome", "prettierd", "prettier" } },
+            },
+        })
+        -- require('conform').formatters.eslint_d = {
+        --     command = "eslint_d",
+        --     prepend_args = function()
+        --         return { "--rule", "'{'space-in-parens": ["error", "always"]}'" }
+        --     end,
+        -- }
+    end
 }
